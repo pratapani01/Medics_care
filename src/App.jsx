5c9d94f76a3f53c2575a305db5a1e17898f274dd
pratapani01 <pratapani01@gmail.com>
2025-12-11 11:32:27 +0530

    scroll fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Doctors from './pages/Doctors';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 import Layout from './components/Layout/Layout';
+import ScrollToTop from './components/Utils/ScrollToTop'; 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="doctors" element={<Doctors />} />
-        <Route path="services" element={<Services />} />
-        <Route path="contact" element={<Contact />} />
-        {/* Add more routes if necessary */}
-      </Route>
-    </Routes>
+    <> 
+      <ScrollToTop /> 
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="doctors" element={<Doctors />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
